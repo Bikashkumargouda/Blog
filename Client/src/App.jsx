58541f2ef5764e52072bcb258f868f5ab4b944cc
bikash kumar gouda <kumar.bikash.gouda@gmail.com>
2024-12-09 12:21:31 +0530

    Create Multipages and Routes

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Dashboard from "./pages/Dashboard"
+import Projects from "./pages/Projects"
+import Signin from "./pages/Signin"
+import Signup from "./pages/Signup"
+
 
 const App = () => {
   return (
-    <div className='text-9xl'>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/projects' element={<Projects />} />
+        <Route path='/signin' element={<Signin />} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='*' element={"ERROR"} />
+
+      </Routes>
+    </BrowserRouter>
   )
 }
 
