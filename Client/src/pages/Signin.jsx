bd0af2ecfe14fc3a321a9dbf04ac082b3efee3fa
bikash kumar gouda <kumar.bikash.gouda@gmail.com>
2024-12-16 08:58:37 +0530

    Add Redux toolkit

--- Client/src/pages/Signin.jsx
+++ Client/src/pages/Signin.jsx
@@ -2,26 +2,30 @@
 import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { signInStart, signInFailure, signInSuccess } from "../redux/user/userSlice";
 
 const Signin = () => {
   const [formData, setFormData] = useState({});
-  const [errorMessage, setErrorMessage] = useState(null);
-  const [loading, setLoading] = useState(false);
+
+  const { loading, error: errorMessage } = useSelector(state => state.user)
+
+  const dispatch = useDispatch();
+
   const navigate = useNavigate();
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
-    setErrorMessage(null); // Reset error message on input change
+
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     if (!formData.password || !formData.email) {
-      return setErrorMessage("Please fill out all fields!");
+      return dispatch(signInFailure("Please fill out all fields!"))
     }
     try {
-      setLoading(true);
-      setErrorMessage(null);
+      dispatch(signInStart());
       const response = await fetch("/api/auth/signin", {
         method: "POST",
         headers: {
@@ -31,16 +35,15 @@ const Signin = () => {
       });
       const data = await response.json();
       if (!response.ok) {
-        throw new Error(data.message || `HTTP error! status: ${response.status}`);
+        dispatch(signInFailure(data.message))
       }
-      setLoading(false);
       setFormData({});
       if (response.ok) {
+        dispatch(signInSuccess(data));
         navigate("/")
       }
     } catch (error) {
-      setErrorMessage(error.message);
-      setLoading(false);
+      dispatch(signInFailure(error.message))
     }
   };
 
